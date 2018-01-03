20f7f76ffd66681656bd25cf39f74e06d6ad02a3
lfernandezaragon <lucasfernandezaragon@gmail.com>
2018-01-03 13:49:57 +0100

    Initial Commit

---
+++ ultrastar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ultrastar
+//
+//  Created by lucas fernández on 03/01/2018.
+//  Copyright © 2018 lucas fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
